c74298070174494f004d16c6387b14260ef5399a
tuvalBarakWSC <tuval.barak@wsc-sports.com>
2023-10-18 14:44:46 +0300

    Targeting SDK version 0.1.5.

--- app/src/main/java/com/wscsports/android/blaze/sampleapp/Application.kt
+++ app/src/main/java/com/wscsports/android/blaze/sampleapp/Application.kt
@@ -14,7 +14,7 @@ class Application : Application() {
 
         BlazeSDK.init(
             // Please provide valid API-KEY here
-            apiKey = "[API-KEY]",
+            apiKey = "[API_KEY]",
 
             application = this,
             cachingLevel = CachingLevel.DEFAULT,
